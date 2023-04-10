a894ad7b27ff2584cd2e6e6172b6d7460d18c78e
Farhan Ishrak Islam <niloy28@live.com>
2023-04-11 00:55:38 +0600

    Move fetch to action creator thunk.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { uiActions } from "./store/ui";
+import { saveCartToDB, fetchCartFromDB } from "./store/cart";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
+
+let isInitialLoad = true;
 
 const DB_URL =
 	"https://react-http-60166-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json";
@@ -14,45 +16,19 @@ function App() {
 	const showCart = useSelector((state) => state.ui.showCart);
 	const cart = useSelector((state) => state.cart);
 	const notification = useSelector((state) => state.ui.notification);
-	const notificationDispatch = useDispatch();
+	const dispatch = useDispatch();
 
 	useEffect(() => {
-		const saveCartToDB = async () => {
-			notificationDispatch(
-				uiActions.showNotification({
-					status: "pending",
-					title: "Sending...",
-					message: "Sending cart data!",
-				})
-			);
-			const response = await fetch(DB_URL, {
-				method: "PUT",
-				body: JSON.stringify(cart),
-			});
+		if (isInitialLoad) {
+			isInitialLoad = false;
+			dispatch(fetchCartFromDB());
+			return;
+		}
 
-			if (!response.ok) {
-				throw new Error("Sending cart data failed!");
-			}
-
-			notificationDispatch(
-				uiActions.showNotification({
-					status: "success",
-					title: "Sent",
-					message: "Successfully sent cart data!",
-				})
-			);
-		};
-
-		saveCartToDB().catch((error) => {
-			notificationDispatch(
-				uiActions.showNotification({
-					status: "error",
-					title: "Error",
-					message: "Sending cart data failed!",
-				})
-			);
-		});
-	}, [cart]);
+		if (cart.changed) {
+			dispatch(saveCartToDB(cart));
+		}
+	}, [cart, dispatch]);
 
 	return (
 		<>
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,15 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit/dist";
+import { uiActions } from "./ui";
 
 const initialState = {
 	orders: [],
 	totalQuantity: 0,
 	totalPrice: 0,
+	changed: false,
 };
+
+const DB_URL =
+	"https://react-http-60166-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json";
 
 const cartSlice = createSlice({
 	name: "cart",
 	initialState,
 	reducers: {
+		replace(state, action) {
+			state.orders = action.payload.orders;
+			state.totalPrice = action.payload.totalPrice;
+			state.totalQuantity = action.payload.totalQuantity;
+		},
 		add(state, action) {
 			const existingOrder = state.orders.find(
 				(order) => order.id === action.payload.id
@@ -29,6 +39,7 @@ const cartSlice = createSlice({
 			}
 			state.totalPrice += action.payload.price;
 			state.totalQuantity++;
+			state.changed = true;
 		},
 		remove(state, action) {
 			const existingOrder = state.orders.find(
@@ -44,12 +55,90 @@ const cartSlice = createSlice({
 			}
 			state.totalPrice -= action.payload.price;
 			state.totalQuantity--;
+			state.changed = true;
 		},
 		toggleCart(state) {
 			state.show = !state.show;
 		},
 	},
 });
+
+export const fetchCartFromDB = () => {
+	return async (dispatch) => {
+		const fetchData = async () => {
+			const response = await fetch(DB_URL);
+
+			return await response.json();
+		};
+
+		try {
+			const cart = await fetchData();
+
+			dispatch(
+				cartActions.replace({
+					orders: cart.orders || [],
+					totalPrice: cart.totalPrice || 0,
+					totalQuantity: cart.totalQuantity || 0,
+				})
+			);
+		} catch (error) {
+			dispatch(
+				uiActions.showNotification({
+					status: "error",
+					title: "Error",
+					message: "Fetching cart data failed!",
+				})
+			);
+		}
+	};
+};
+
+export const saveCartToDB = (cart) => {
+	return async (dispatch) => {
+		dispatch(
+			uiActions.showNotification({
+				status: "pending",
+				title: "Sending...",
+				message: "Sending cart data!",
+			})
+		);
+
+		const sendRequest = async () => {
+			const response = await fetch(DB_URL, {
+				method: "PUT",
+				body: JSON.stringify({
+					orders: cart.orders,
+					totalPrice: cart.totalPrice,
+					totalQuantity: cart.totalQuantity,
+				}),
+			});
+
+			if (!response.ok) {
+				throw new Error("Sending cart data failed!");
+			}
+		};
+
+		try {
+			await sendRequest();
+
+			dispatch(
+				uiActions.showNotification({
+					status: "success",
+					title: "Sent",
+					message: "Successfully sent cart data!",
+				})
+			);
+		} catch (error) {
+			dispatch(
+				uiActions.showNotification({
+					status: "error",
+					title: "Error",
+					message: "Sending cart data failed!",
+				})
+			);
+		}
+	};
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
